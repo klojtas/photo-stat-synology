21be54762122e93eb53ef73d760a9861f611a213
klojtas <w.gasiorowski@gmail.com>
2023-01-22 19:31:08 +0100

    Update photo-stat.sh

--- photo-stat.sh
+++ photo-stat.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 declare -a arr=("05" "06" "07" "08" "05" "06" "07" "08" "09" "10" "11" "12" "13" "14" "15" "16" "17" "18" "19" "20" "21" "22" "23")
+MIN_SIZE="500k"
+FOLDER_PREFIX="rok 20"
 for i in ${arr[@]}; do
-  FOLDER_NAME="rok 20$i"
+  FOLDER_NAME="$FOLDER_PREFIX$i"
   echo "$FOLDER_NAME"
-	find "$FOLDER_NAME"  -size +1M -type f | wc -l
+  find "$FOLDER_NAME"  -size "+$MIN_SIZE" -type f | wc -l
 done 
